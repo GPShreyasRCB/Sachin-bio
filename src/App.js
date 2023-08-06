e3edc6d34034ae653809aab1cfa84cd6cf153700
Shreyas GP <shreyasgp041@gmail.com>
2023-08-06 12:55:00 +0900

    Add Sachin details

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+const App = () => {
+  return (
+    <div>
+       <h1>Meet My Friend: Sachin Hittalamani</h1>
+      <img src="image.png" alt="Description of the image" />
+      <p>
+        Sachin Hittalamani is an accomplished Test Engineer at Cureus Technologies,
+        making valuable contributions to the team. With a full-time commitment, he
+        showcases a keen passion for quality assurance, meticulously assessing and
+        enhancing software products. His role as a Test Engineer demonstrates his dedication
+        to ensuring the reliability, functionality, and performance of applications.
+        Sachin's diligent work ethic, attention to detail, and problem-solving aptitude
+        contribute significantly to the success of projects he undertakes.
+      </p>
+    </div>
+  );
+};
+
+export default App;
